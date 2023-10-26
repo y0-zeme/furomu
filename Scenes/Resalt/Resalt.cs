31325d8258b63f5155ca1182d5e8b57710b505db
haniwa555 <143782035+haniwa555@users.noreply.github.com>
2023-10-26 19:20:42 +0900

    ゲームオーバーシーンの追加

---
+++ Scenes/Resalt/Resalt.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Resalt : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
