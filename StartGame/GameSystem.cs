7931890bc1fb8a040849f139fa1ca9f26389fe19
haniwa555 <143782035+haniwa555@users.noreply.github.com>
2023-11-02 18:14:58 +0900

    Add files via upload

---
+++ StartGame/GameSystem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour
+{
+   public void StartGame() {
+        SceneManager.LoadScene("2.ŽR");
+   }
+}
---
+++ Time/SceneMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    private float step_time;    // 経過時間カウント用
+
+    // Use this for initialization
+    void Start()
+    {
+        step_time = 0.0f;       // 経過時間初期化
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 経過時間をカウント
+        step_time += Time.deltaTime;
+
+        // 30秒後に画面遷移（scene2へ移動）
+        if (step_time >= 30.0f)
+        {
+            SceneManager.LoadScene("3.村");
+        }
+    }
+}
